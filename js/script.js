83527984e27857028cb04f36e91cf5aa56d34a99
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-02-12 21:05:45 +0100

    timer
    timer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-// JS file
+// JS file var seconds;
+//timer
+ <script type="text/javascript">
+  var seconds;
+  var temp;
+ 
+  function countdown() {
+    seconds = document.getElementById('countdown').innerHTML;
+    seconds = parseInt(seconds, 10);
+ 
+    if (seconds == 1) {
+      temp = document.getElementById('countdown');
+      temp.innerHTML = "Done";
+      return;
+    }
+ 
+    seconds--;
+    temp = document.getElementById('countdown');
+    temp.innerHTML = seconds;
+    timeoutMyOswego = setTimeout(countdown, 1000);
+  } 
+ 
+  countdown();
+</script>
