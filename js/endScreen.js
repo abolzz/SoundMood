f05f1a8dfa5a7dbf1c3bf206cd3fbb2b0f2d9a7c
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-02-12 18:58:36 +0100

    Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/abolzz/SoundMood""""""""""""""""""""""
    This reverts commit 8600742e8c50d1aeb9a8ba6a64280e87325edfed.

---
+++ js/endScreen.js
@@ -0,0 +1,21 @@
+//endScreen
+/*var endScreen = document.getElementById('finalScreen').style.display = 'none';
+
+for (let i = 0; i < imgEmoji.length; i++) {
+  imgEmoji[i].onclick = function() {
+    //endScreen.style.display = 'block';
+    endScreen.show();
+  }
+}
+*/
+//experiment
+/*for (let i = 0; i < imgEmoji.length; i++) {
+  imgEmoji.onclick = function() {
+    var endScreen = document.getElementById('finalScreen');
+    if (endScreen.style.display !== 'none') {
+      endScreen.style.display = 'none';
+    } else {
+      endScreen.style.display = 'block';
+    }
+  };
+}*/
