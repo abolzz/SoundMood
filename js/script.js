5ec9a85f0e64df4fe01744cd1c4a179a5c83df47
pjm90 <33023671+pjm90@users.noreply.github.com>
2018-02-13 10:56:26 +0100

    section flow
    section flow

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // JS file var seconds;
 
-  var seconds;
+ 
+
+document.getElementById('ready').onclick = function(){
+  document.getElementById('loadingscreen').classList.add('hidden');
+} ;
+
+document.getElementById('ready2').onclick = function(){
+  document.getElementById('instruction1').classList.add('hidden');
+   var seconds;
   var temp;
  
   function countdown() {
@@ -11,16 +19,13 @@
       temp = document.getElementById('countdown');
       temp.innerHTML = "Done";
       return;
-    }
+    };
  
     seconds--;
     temp = document.getElementById('countdown');
     temp.innerHTML = seconds;
     timeoutMyOswego = setTimeout(countdown, 1000);
-  } 
+  } ;
  
   countdown();
-
-document.getElementById('ready').onclick = function(){
-  document.getElementById('loadingscreen').classList.add('hidden');
 } ;
