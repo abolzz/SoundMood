9e7e012c6786e8d4f0e4bffef73bc9b37ba96932
mariaasenova <mariaasenova22@gmail.com>
2018-02-13 12:02:21 +0100

    play/pause buttons

---
+++ js/mariaScript.js
@@ -0,0 +1,57 @@
+/*var myAudio = document.getElementById('myAudio');
+var audio = document.getElementsByTagName('audio');
+
+
+function playAudio() {
+  myAudio.play();
+}
+
+function pauseAudio() {
+  myAudio.pause();
+}*/
+
+var audio = document.getElementsByTagName('audio');
+var playButton = document.getElementsByClassName('endScreen__button--play');
+var pauseButton = document.getElementsByClassName('endScreen__button--pause');
+
+document.getElementById('playAudioSlow').onclick = function playAudioSlow() {
+  for (var i = 0; i < audio.length; i++) {
+    audio[0].play();
+  }
+}
+
+document.getElementById('playAudioDay').onclick = function playAudioDay() {
+  for (var i = 0; i < audio.length; i++) {
+    audio[1].play();
+  }
+}
+
+document.getElementById('playAudioHappy').onclick = function playAudioHappy() {
+  for (var i = 0; i < audio.length; i++) {
+    audio[2].play();
+  }
+
+}
+
+function pauseAudio() {
+  for (var i = 0; i < audio.length; i++) {
+    audio[i].pause();
+  }
+}
+
+$(document).ready(function() {
+  var audioElement = document.getElementById('audio');
+  $('#pauseButtonImg').hide();
+
+  $('#playButton').click(function() {
+    $('#playButtonImg').hide();
+    $('#pauseButtonImg').show();
+    audioElement.play();
+  });
+
+  $('#pauseButton').click(function() {
+    $('#playButtonImg').show();
+    $('#pauseButtonImg').hide();
+    audioElement.pause();
+  });
+});
