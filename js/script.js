55feca1bc6151fb665a547407846ad325e5cab34
mariaasenova <mariaasenova22@gmail.com>
2018-02-14 12:28:29 +0100

    buttonSound

--- js/script.js
+++ js/script.js
@@ -1,35 +1,41 @@
+var soundEffectAudio = document.getElementById('buttonSound');
+soundEffectAudio.src = "songs/click.mp3";
+
+
 // JS file var seconds;
 
-document.getElementById('ready').onclick = function(){
+document.getElementById('ready').onclick = function() {
   document.getElementById('loadingscreen').classList.add('hidden');
+  soundEffectAudio.play();
   // animation
   document.getElementById("instructionP").classList.add("zoomIn");
   document.getElementById("ok").classList.add("zoomIn");
-} ;
- 
- // first selection round timer
-$("#ok").click(function(){
+};
+
+// first selection round timer
+$("#ok").click(function() {
   $('#timer').addClass('timer-quick');
   $('.timer-quick').startTimer();
-    console.log('timer works');
+  console.log('timer works');
 });
 
 // second selection round timer
-$("#ok2").click(function(){
+$("#ok2").click(function() {
   $('#timer2').addClass('timer-quick');
   $('.timer-quick').startTimer();
-    console.log('timer works');
+  console.log('timer works');
 });
 
 var error;
 
-document.getElementById('ok').onclick = function(){
+document.getElementById('ok').onclick = function() {
   document.getElementById('instruction1').classList.add('hidden');
-    // after 10 seconds error screen appears
-  error = setTimeout(function(){
+  soundEffectAudio.play();
+  // after 10 seconds error screen appears
+  error = setTimeout(function() {
     document.getElementById("SlowEndScreen").classList.add("topLayer");
-   }, 10000);
-} ;
+  }, 10000);
+};
 
 var result = "slow";
 
@@ -38,17 +44,18 @@ console.log(result);
 
 // first round of selections - picture selection
 for (var i = 0; i < firstRound.length; i++) {
-  firstRound[i].onclick = function (event) {
+  firstRound[i].onclick = function(event) {
     var value = event.target.getAttribute('data-value');
     console.log(value);
     result = value;
     console.log(result);
-        // because the user clicked on one of the images the timeOut for error screen gets cleared
+    soundEffectAudio.play();
+    // because the user clicked on one of the images the timeOut for error screen gets cleared
     clearTimeout(error);
     document.getElementById("gallery1").classList.add("hidden");
-    // makes the gifScreen disappear after 10 seconds 
-    setTimeout(function(){
-     document.getElementById("gifScreen").classList.add("hidden");
+    // makes the gifScreen disappear after 10 seconds
+    setTimeout(function() {
+      document.getElementById("gifScreen").classList.add("hidden");
       // animation
       document.getElementById("instructionP2").classList.add("zoomIn");
       document.getElementById("ok2").classList.add("zoomIn");
@@ -56,34 +63,36 @@ for (var i = 0; i < firstRound.length; i++) {
   }
 };
 
-document.getElementById('ok2').onclick = function(){
+document.getElementById('ok2').onclick = function() {
   document.getElementById('instruction2').classList.add('hidden');
-      // after 10 seconds error screen appears
-  error = setTimeout(function(){
+  soundEffectAudio.play();
+  // after 10 seconds error screen appears
+  error = setTimeout(function() {
     document.getElementById("SlowEndScreen").classList.add("topLayer");
-   }, 10000);
-} ;
+  }, 10000);
+};
 
 new Vue({
   el: '#gallery2',
   methods: {
-    showResult: function () {
-    var value = event.target.getAttribute('data-value');
-    console.log(value);
-    // combination of both selections (concatinated)
-    result = result + value;
-    console.log(result);
-    // result calculation
+    showResult: function() {
+      var value = event.target.getAttribute('data-value');
+      console.log(value);
+      // combination of both selections (concatinated)
+      result = result + value;
+      console.log(result);
+      soundEffectAudio.play();
+      // result calculation
       if (result == "happyhappy" || result == "happyenergetic" || result == "energetichappy" || result == "energeticenergetic") {
         document.getElementById("HappyEndScreen").classList.add("topLayer");
         console.log(result);
-      }   else if (result == "sadsad" || result == "sadbored" || result == "boredsad" || result == "boredbored") {
+      } else if (result == "sadsad" || result == "sadbored" || result == "boredsad" || result == "boredbored") {
         document.getElementById("SadEndScreen").classList.add("topLayer");
         console.log(result);
-        }   else {
+      } else {
         document.getElementById("SlowEndScreen").classList.add("topLayer");
         console.log(result);
-          }
+      }
     }
   }
 });
@@ -97,38 +106,31 @@ console.log(audio);
 
 $(document).ready(function() {
   $(document.getElementsByClassName('endScreen__button--pause')).hide();
-    for (var i = 0; i < playButtons.length; i++) {
-      playButtons[i].onclick = function (event) {
-        var value = $(this).attr('data-value');
-        $(document.getElementsByClassName('endScreen__button--play')).hide();
-        $(document.getElementsByClassName('endScreen__button--pause')).show();
-          if (value == 1){
-          audio[i].play();
-          } else {
-            audio[i+2].play();
-          }
+  for (var i = 0; i < playButtons.length; i++) {
+    playButtons[i].onclick = function(event) {
+      var value = $(this).attr('data-value');
+      $(document.getElementsByClassName('endScreen__button--play')).hide();
+      $(document.getElementsByClassName('endScreen__button--pause')).show();
+      if (value == 1) {
+        audio[i].play();
+      } else {
+        audio[i + 2].play();
       }
     }
+  }
 
-    for (var i = 0; i < pauseButtons.length; i++) {
-      pauseButtons[i].onclick = function (event) {
-        var value = $(this).attr('data-value');
-        $(document.getElementsByClassName('endScreen__button--pause')).hide();
-        $(document.getElementsByClassName('endScreen__button--play')).show();
-          if (value == 1){
-          audio[i].pause();
-          } else {
-            audio[i+2].pause();
-          }
+  for (var i = 0; i < pauseButtons.length; i++) {
+    pauseButtons[i].onclick = function(event) {
+      var value = $(this).attr('data-value');
+      $(document.getElementsByClassName('endScreen__button--pause')).hide();
+      $(document.getElementsByClassName('endScreen__button--play')).show();
+      if (value == 1) {
+        audio[i].pause();
+      } else {
+        audio[i + 2].pause();
       }
     }
+  }
 });
 
-//sound effect//
-var soundEffect = document.getElementsByClassName("instruction__button");
-var = soundEffectAudio = document.getElementsByClassName("")
-for(var i = 0; i < soundEffect.length; i++){
-soundEffect[i].onclick = function(event) {
-
-}
-}
+//sound effect///
